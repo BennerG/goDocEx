96b2a487f6c1e95b815aabbbe5a90ce234f02aa4
BennerG <bgurrola.btg@gmail.com>
2020-11-14 01:44:33 -0700

    test package for exercise

---
+++ godocex/main.go
@@ -0,0 +1,7 @@
+// Package godocex is an learning exercise to practice documentation
+package godocex
+
+// DogYears returns an equivalent age in dog years for a given number of human years
+func DogYears(humanYears int) int {
+	return humanYears * 7
+}
